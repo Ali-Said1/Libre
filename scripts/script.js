93b4113fc97f7687c87ef2f980e5cdbc759e1477
abc <abc@example.com>
2025-10-02 15:37:36 +0300

    syntax edit

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,24 @@
- let x = document.getElementsByClassName("bookCard");
+ let cards = document.getElementsByClassName("bookCard");
 
 
 function search(){
     let input = document.getElementById("searchInput").value.toLowerCase();
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
+    for (i = 0; i < cards.length; i++) { 
+        if (!cards[i].innerHTML.toLowerCase().includes(input)) {
+            cards[i].style.display="none";
         }
         else {
-            x[i].style.display="block";                 
+            cards[i].style.display="block";                 
         }}
 
 }
 
 
 function filterBooks(category) {
-    for (i = 0; i < x.length; i++) { 
-        if (category === "all" || x[i].classList.contains(category)) {
-            x[i].style.display="block";                 
+    for (i = 0; i < cards.length; i++) { 
+        if (category === "all" || cards[i].classList.contains(category)) {
+            cards[i].style.display="block";                 
         } else {
-            x[i].style.display="none";
+            cards[i].style.display="none";
         }}
     }
-    
