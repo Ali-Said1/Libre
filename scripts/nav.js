dbcfcd3faa52aaab513eb34514a6cd6353d8d3b1
abc <abc@example.com>
2025-10-05 17:48:35 +0300

    sign in up wishlist styles , burger menu

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,47 +3,95 @@ let logCheck = false;
 // const { ok } = await authUser();
 // if (ok) logCheck = true;
 
-// function logoutLogic(e) {
-//   e.preventDefault()
-//   fetch('http://localhost:5000/logout', { method: "POST", credentials: 'include' }).then(window.location.reload())
+function logoutLogic(e) {
+  e.preventDefault();
+  fetch('http://localhost:5000/logout', { method: "POST", credentials: 'include' })
+    .then(response => {
+        if(response.ok) {
+            window.location.reload();
+        } else {
+            console.error("Logout failed with status:", response.status);
+        }
+    })
+    .catch(error => {
+        console.error("An error occurred during logout:", error);
+    });
 
-//   return false;
-// }
+  return false;
+}
 
-let nv = `
-<ul class="nv nav-pills p-2 w-100 d-flex align-items-center list-unstyled" style="color: rgb(224, 32, 32) !important;">
-  <li class="nav-item mx-3">
-    <a class="nav-link" href="index.html">Home</a>
-  </li>
- <li class="nav-item mx-3">
-    <a class="nav-link" href="wishlist.html">Wishlist</a>
-  </li>
-  <li class="nav-item ms-auto mx-2">
-    <a href="carts.html" style="color: rgb(224, 32, 32); font-size: 1.4rem">
-      <i class="bi bi-cart"></i>
-    </a>
-  </li>
-  <li class="nav-item mx-3">
-    <a class="nav-link" href="products.html">Products</a>
-  </li>
-  ${logCheck ? `
-  <li class="nav-item mx-3">
-    <a class="nav-link" href="profile.html">Profile</a>
-  </li>
-  <li id="logoutButton" class="nav-item mx-3">
-    <a class="nav-link" href="#">Log Out</a>
-  </li>
-  ` : `
-  <li class="nav-item mx-3">
-    <a class="nav-link" href="signin.html">Login</a>
-  </li>
-  <li class="nav-item mx-3">
-    <a class="nav-link" href="signup.html">Register</a>
-  </li>
-  `}
-</ul>
+const navbarHTML = `
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+    <div class="container-fluid">
+        <!-- GROUP 1: Always-visible left-side links -->
+        <div class="d-flex align-items-center">
+            <ul class="navbar-nav flex-row">
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="wishlist.html">Wishlist</a>
+                </li>
+            </ul>
+        </div>
+
+       
+        <div class="mx-auto">
+             <a href="carts.html" class="nav-link text-danger fs-4">
+                <i class="bi bi-cart"></i>
+            </a>
+        </div>
+        
+        <!-- GROUP 3: Burger menu button for the right-side links -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <!-- Collapsible content for the right-side links -->
+        <div class="collapse navbar-collapse flex-grow-0" id="mainNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="products.html">Products</a>
+                </li>
+
+                <!-- Conditional links based on login status -->
+                ${logCheck ? `
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="profile.html">Profile</a>
+                </li>
+                <li id="logoutButton" class="nav-item">
+                    <a class="nav-link text-danger" href="#" style="cursor: pointer;">Log Out</a>
+                </li>
+                ` : `
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="signin.html">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="signup.html">Register</a>
+                </li>
+                `}
+            </ul>
+        </div>
+    </div>
+</nav>
 `;
 
-document.body.insertAdjacentHTML("afterbegin", nv);
+const ensureBootstrapIcons = () => {
+    if (!document.querySelector('link[href*="bootstrap-icons"]')) {
+        const iconsLink = document.createElement('link');
+        iconsLink.rel = 'stylesheet';
+        iconsLink.href = 'node_modules/bootstrap-icons/font/bootstrap-icons.css';
+        document.head.appendChild(iconsLink);
+    }
+};
 
-document.getElementById('logoutButton')?.addEventListener('click', logoutLogic)
+
+ensureBootstrapIcons();
+
+document.body.insertAdjacentHTML("afterbegin", navbarHTML);
+
+const logoutButton = document.getElementById('logoutButton');
+if (logoutButton) {
+    logoutButton.addEventListener('click', logoutLogic);
+}
+
